83962a20d92af444112a42223f56f1efb05e2260
Ankita <ankita@ajency.in>
2019-09-10 15:50:18 +0530

    landmarkgoavilla theme files

---
+++ wp-content/themes/landmarkgoavillas-child-theme/functions.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template includes necessary functions for theme.
+ *
+ * @package pado
+ * @since 1.0
+ */
+
+
+if (! function_exists('pado_child_styles')) {
+	function pado_child_styles(){
+
+		// register style
+		wp_enqueue_style('pado-child-css', get_stylesheet_directory_uri() . '/style.css');
+
+
+	}
+}
+add_action( 'wp_enqueue_scripts', 'pado_child_styles');
+
+function footer_widgets_init() {
+	register_sidebar( array(
+		'name' => 'Footer Sidebar 1',
+		'id' => 'footer-sidebar-1',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	register_sidebar( array(
+		'name' => 'Footer Sidebar 2',
+		'id' => 'footer-sidebar-2',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	register_sidebar( array(
+		'name' => 'Footer Sidebar 3',
+		'id' => 'footer-sidebar-3',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+}
+add_action('widgets_init','footer_widgets_init');

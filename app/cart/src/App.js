de15467fb514449499210620a2662adb1fceb52a
Vaibhav Arolkar <vaibhav.a@ajency.in>
2019-10-18 18:54:27 +0530

    Cart UI (incomplete)

--- app/cart/src/App.js
+++ app/cart/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import './assets/scss/main.scss';
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 import Cart from './components/cart/cart.js';
 

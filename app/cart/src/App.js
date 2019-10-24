b5d1b66256993c3296b27e7bda17faf00b533272
latesh <latesh@ajency.in>
2019-10-24 10:32:39 +0530

    display map in app

--- app/cart/src/App.js
+++ app/cart/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import './assets/scss/main.scss';
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 import Cart from './components/cart/cart.js';
+import AddNewAddress from './components/add-new-addess/add-new-address';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <AddNewAddress latlng={{lat:15.487590683051524,lng:73.83213189817026}} address={"Panjim Convention Centre, Panjim goa"}/>
           <Cart/> 
       </div>
     );

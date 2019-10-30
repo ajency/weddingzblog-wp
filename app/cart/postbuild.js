e8bf0b031a8030eae466364e0861c3b0f54f4600
AJ-Sujit <sujit@ajency.in>
2019-10-30 08:16:59 +0530

    updated post build script to store react css file hashes

--- app/cart/postbuild.js
+++ app/cart/postbuild.js
@@ -1,7 +1,9 @@
 const fs = require('fs-extra');
 
 let react_file_hash = {};
+let react_css_file_hash = {}
 let js_files = ["runtime-main.", "main.", "2."];
+let css_files = ["main."];
 
 fs.emptyDir('../../wp-content/themes/ajency-portfolio/js/cart')
 .then(() => {
@@ -9,12 +11,15 @@ fs.emptyDir('../../wp-content/themes/ajency-portfolio/js/cart')
 		.then((success) =>{
 			console.log("build folder copied successfully");
 			for(let i = 0; i<js_files.length; i++){
-			    fromDir('./build/static/js/', js_files[i]);
+			    fromDir('./build/static/js/', js_files[i], 'js');
 			}
+
+			fromDir('./build/static/css/', css_files[0], 'css');
+
 
 			// fromDir('../../wp-content/themes/ajency-portfolio/js/cart/static/css/', "main.");
 
-			console.log("file hash ==>", react_file_hash);
+			console.log("file hash ==>", react_file_hash, react_css_file_hash);
 
 			fs.writeJson('../../wp-content/themes/ajency-portfolio/react_file_hash.json', react_file_hash)
 			.then(() => {
@@ -23,6 +28,15 @@ fs.emptyDir('../../wp-content/themes/ajency-portfolio/js/cart')
 			.catch(err => {
 			  console.error(err)
 			})
+
+			fs.writeJson('../../wp-content/themes/ajency-portfolio/react_css_file_hash.json', react_css_file_hash)
+			.then(() => {
+			  console.log('success!')
+			})
+			.catch(err => {
+			  console.error(err)
+			})
+
 		})
 		.catch((error)=>{
 			console.log("error in copying build folder");
@@ -34,17 +48,28 @@ fs.emptyDir('../../wp-content/themes/ajency-portfolio/js/cart')
 
 
 
-function fromDir(startPath,filter){
+function fromDir(startPath,filter, type){
     if (!fs.existsSync(startPath)){
         console.log("no dir ",startPath);
         return;
     }
+    console.log("type ==>", type);
 
     var files=fs.readdirSync(startPath);
-    for(var i=0;i<files.length;i++){
-        if (files[i].indexOf(filter)>=0) {
-            console.log('-- found: ',files[i]);
-            react_file_hash[files[i].split('.')[0]] = files[i].split('.')[1];
-        };
-    };
+    if(type == 'js'){
+    	for(var i=0;i<files.length;i++){
+	        if (files[i].indexOf(filter)>=0) {
+	            console.log('-- found: ',files[i]);
+	            react_file_hash[files[i].split('.')[0]] = files[i].split('.')[1];
+	        };
+	    };
+    }
+    else {
+    	for(var i=0;i<files.length;i++){
+	        if (files[i].indexOf(filter)>=0) {
+	            console.log('-- found: ',files[i]);
+	            react_css_file_hash[files[i].split('.')[0]] = files[i].split('.')[1];
+	        };
+	    };
+    }
 };

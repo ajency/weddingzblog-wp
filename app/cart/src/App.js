dac64e167d8969fbd5dcbe741645d0515b6aad51
AJ-Sujit <sujit@ajency.in>
2019-10-30 11:26:37 +0530

    component for list of addresses

--- app/cart/src/App.js
+++ app/cart/src/App.js
@@ -4,13 +4,16 @@ import './assets/scss/main.scss';
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 import Cart from './components/cart/cart.js';
 import AddNewAddress from './components/add-new-addess/add-new-address';
+import AddressList from './components/address-list/address-list';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <AddNewAddress latlng={{lat:15.487590683051524,lng:73.83213189817026}} address={"Panjim Convention Centre, Panjim goa"}/>
-          <Cart/> 
+        {/*  <AddNewAddress latlng={{lat:15.487590683051524,lng:73.83213189817026}} address={"Panjim Convention Centre, Panjim goa"}/> /*}
+        {/* <Cart/> */}
+
+        <AddressList/>
       </div>
     );
   }

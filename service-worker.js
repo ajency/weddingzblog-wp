2ebe3f8d8cb3466a4f2709351846a1261fac5e94
AJ-Sujit <sujit@ajency.in>
2019-11-05 18:37:29 +0530

    configuration for caching react js and css files

--- service-worker.js
+++ service-worker.js
@@ -17,12 +17,59 @@ workbox.routing.registerRoute(
             statuses: [0, 200],
         }),
         new workbox.expiration.Plugin({
-            maxAgeSeconds: 60 * 4, // cache for one week
+            maxAgeSeconds: 60 * 60 * 24,
             maxEntries: 50,
             purgeOnQuotaError: true
         })
     ]
   })
+);
+
+
+//cache js and css - production
+workbox.routing.registerRoute(
+    new RegExp('https://order.greengrainbowl.com/*'),
+    workbox.strategies.cacheFirst({
+        cacheName: 'ggb-prod',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 7, // cache for one week
+                maxEntries: 50, 
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+
+//cache js and css - staging
+workbox.routing.registerRoute(
+    new RegExp('http://order-staging.greengrainbowl.com/*'),
+    workbox.strategies.cacheFirst({
+        cacheName: 'ggb-stage',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 7, // cache for one week
+                maxEntries: 50, 
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+//cache js and css - local
+workbox.routing.registerRoute(
+    new RegExp('/greengrainbowl/app/build/*'),
+    workbox.strategies.cacheFirst({
+        cacheName: 'ggb-local',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 7, // cache for one week
+                maxEntries: 50, 
+                purgeOnQuotaError: true
+            })
+        ]
+    })
 );
 
 workbox.precaching.precacheAndRoute([]);
